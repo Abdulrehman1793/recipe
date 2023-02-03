ef0e47536bdd28be224a9737ad147a457735d28c
KHAN ABDULREHMAN <abdulrehman1793@gmail.com>
2023-02-03 15:34:29 +0530

    Complete IT for uom

--- src/test/resources/scripts/INIT_UOM.sql
+++ src/test/resources/scripts/INIT_UOM.sql
@@ -1,2 +1,2 @@
-INSERT INTO unit_of_measure(uom, version)
-values ('teaspoon', 1),('pinch', 1),('cup', 1),('gram', 1),('kilogram', 1);
+INSERT INTO unit_of_measure(id, uom, version)
+values (1, 'teaspoon', 1),(2, 'pinch', 1),(3, 'cup', 1),(4, 'gram', 1),(5, 'kilogram', 1);
